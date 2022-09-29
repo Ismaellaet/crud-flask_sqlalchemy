dfbb10a2c55ee3b3c20dfcaa4e63b048242d9945
Ismael de Laet Abashi <ismael.abashi@skyone.solutions>
2022-09-29 15:17:13 -0300

    feat: GET /users

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+import json
+from flask import Flask, Response, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -15,6 +16,32 @@ class User(db.Model):
     name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), nullable=False)
 
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email
+        }
+
+
+class UsersResource(Resource):
+    def get(self):
+        users = User.query.all()
+        users_list = [user.to_json() for user in users]
+        return make_response(200, 'users', users_list)
+
+
+def make_response(status, content_name, content, message=None):
+    body = {}
+    body[content_name] = content
+
+    if message:
+        body['message'] = message
+
+    return Response(json.dumps(body), status=status, mimetype='application/json')
+
+
+api.add_resource(UsersResource, '/users')
 
 if __name__ == '__main__':
     app.run(debug=True)
